98ffca452afd4f04ba0a44f6ffa27b8552a12142
Jason Allen <jasonallen@Jasons-MacBook-Pro.local>
2018-10-03 08:27:10 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<body>
+  <div id="app">
+    <router-link to = "/">Home</router-link>
+    <router-link to = "/CNFacts">Chuck Norris Facts</router-link>
+    <router-view/>
+    <img src="http://www.smosh.com/wp-content/uploads/legacy.images/smosh-pit/022011/chuck-norris-super-kick.gif">
+  </div>
+</body>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body{
+  background-color: #333635;
+}
+a{
+  color: white;
+}
+</style>
---
+++ src/components/CNFacts.vue
@@ -0,0 +1,58 @@
+<template>
+<body>
+  <div id="CNFacts">
+  <h1 id="appTitle">Chuck Norris Facts</h1>
+
+  <div class="input-group mb-3">
+  <input id="searchInput" type="text" class="form-control" aria-label="Recipient's username" aria-describedby="basic-addon2">
+  <div class="input-group-append">
+  <button id="factBtn" type="button">Get Facts!</button>
+  </div>
+</div>
+
+<ul>
+  <li id="factList" ><h2></h2></li>
+</ul>
+
+</div>
+</body>
+
+</template>
+
+<script>
+export default {
+  name: 'CNFacts',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  color: white;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+
+a {
+  color: #42b983;
+}
+
+#factBtn{
+  background-color: #20A600;
+  color: white;
+}
+</style>
